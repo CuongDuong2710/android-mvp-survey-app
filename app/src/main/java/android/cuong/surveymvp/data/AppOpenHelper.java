a424b5da6fd607ae613a7b02ccd18dd30ecc7286
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-11-19 15:57:28 +0700

    creating AppOpenHelper class

---
+++ app/src/main/java/android/cuong/surveymvp/data/AppOpenHelper.java
@@ -0,0 +1,121 @@
+package android.cuong.surveymvp.data;
+
+import android.cuong.surveymvp.data.model.DaoMaster;
+import android.cuong.surveymvp.data.model.DaoSession;
+import android.cuong.surveymvp.data.model.Option;
+import android.cuong.surveymvp.data.model.Question;
+import android.cuong.surveymvp.data.model.User;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
+
+/**
+ * Created by QUOC CUONG on 19/11/2017.
+ */
+
+//TODO: @Singleton
+public class AppOpenHelper implements DbHelper {
+
+    private final DaoSession mDaoSession;
+
+    //TODO: @inject
+    public AppOpenHelper(DbOpenHelper dbOpenHelper) {
+        mDaoSession = new DaoMaster(dbOpenHelper.getWritableDb()).newSession();
+    }
+
+    @Override
+    public Observable<Long> insertUser(final User user) {
+        return Observable.fromCallable(new Callable<Long>() {
+            @Override
+            public Long call() throws Exception {
+                return mDaoSession.getUserDao().insert(user);
+            }
+        });
+    }
+
+    @Override
+    public Observable<List<User>> getAllUsers() {
+        return Observable.fromCallable(new Callable<List<User>>() {
+            @Override
+            public List<User> call() throws Exception {
+                return mDaoSession.getUserDao().loadAll();
+            }
+        });
+    }
+
+    @Override
+    public Observable<List<Question>> getAllQuestions() {
+        return Observable.fromCallable(new Callable<List<Question>>() {
+            @Override
+            public List<Question> call() throws Exception {
+                return mDaoSession.getQuestionDao().loadAll();
+            }
+        });
+    }
+
+    @Override
+    public Observable<Boolean> isQuestionEmpty() {
+        return Observable.fromCallable(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                return !(mDaoSession.getQuestionDao().count() > 0);
+            }
+        });
+    }
+
+    @Override
+    public Observable<Boolean> isOptionEmpty() {
+        return Observable.fromCallable(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                return !(mDaoSession.getOptionDao().count() > 0);
+            }
+        });
+    }
+
+    @Override
+    public Observable<Boolean> saveQuestion(final Question question) {
+        return Observable.fromCallable(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                mDaoSession.getQuestionDao().insert(question);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public Observable<Boolean> saveOption(final Option option) {
+        return Observable.fromCallable(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                mDaoSession.getOptionDao().insert(option);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public Observable<Boolean> saveQuestionList(final List<Question> questionList) {
+        return Observable.fromCallable(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                mDaoSession.getQuestionDao().insertInTx(questionList);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public Observable<Boolean> saveOptionList(final List<Option> optionList) {
+        return Observable.fromCallable(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                mDaoSession.getOptionDao().insertInTx(optionList);
+                return true;
+            }
+        });
+    }
+}
