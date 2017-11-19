1720b8e997d659eaf30a72d5807e09aa3fb9bb8d
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-11-19 08:41:27 +0700

    creating DialogMvpView class

---
+++ app/src/main/java/android/cuong/surveymvp/ui/base/DialogMvpView.java
@@ -0,0 +1,10 @@
+package android.cuong.surveymvp.ui.base;
+
+/**
+ * Created by QUOC CUONG on 19/11/2017.
+ */
+
+public interface DialogMvpView extends MvpView {
+
+    void dismisDialog(String tag);
+}
